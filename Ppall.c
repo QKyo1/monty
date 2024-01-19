21d3d4aaedde707f5290bcd4c17a0b8a91c67277
qkyo1 <engawab8@gmai.com>
2024-01-19 06:20:55 +0400

    ;;;

---
+++ Ppall.c
@@ -0,0 +1,21 @@
+#include "mmonty.h"
+/**
+ * f_pall - a functionn to print the stackk
+ * @head: Head of th33 stackk
+ * @counter: no used for counnt3r
+ * Return: no retur3n
+*/
+void f_pall(stack_t **head, unsigned int counter)
+{
+	stack_t *h;
+	(void)counter;
+
+	h = *head;
+	if (h == NULL)
+		return;
+	while (h)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+	}
+}
